32c41e0e2a2e87c42f9c5d7caafe7ed29005b6e7
Alisa-Bogatyryova <80385825+Alisa-Bogatyryova@users.noreply.github.com>
2021-12-17 16:19:13 +0300

    Updates

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore} from '@reduxjs/toolkit';
-import rootReducer from './contacts-reducer';
-
-
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-export default store;
